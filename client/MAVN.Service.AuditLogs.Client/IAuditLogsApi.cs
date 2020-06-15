3417039e3b241aed8add98526e5a4ac055d5c94c
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-06-15 12:14:40 +0300

    Add action types and initial migration

--- client/MAVN.Service.AuditLogs.Client/IAuditLogsApi.cs
+++ client/MAVN.Service.AuditLogs.Client/IAuditLogsApi.cs
@@ -7,9 +7,6 @@ using Refit;
 
 namespace MAVN.Service.AuditLogs.Client
 {
-    // This is an example of service controller interfaces.
-    // Actual interface methods must be placed here (not in IAuditLogsClient interface).
-
     /// <summary>
     /// AuditLogs client API interface.
     /// </summary>
---
+++ src/MAVN.Service.AuditLogs.MsSqlRepositories/Migrations/20200615091154_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MAVN.Service.AuditLogs.MsSqlRepositories.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "audit_logs");
+
+            migrationBuilder.CreateTable(
+                name: "audit_logs",
+                schema: "audit_logs",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    admin_user_id = table.Column<Guid>(nullable: false),
+                    action_context = table.Column<string>(nullable: true),
+                    timestamp = table.Column<DateTime>(nullable: false),
+                    action_type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_audit_logs", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_audit_logs_admin_user_id",
+                schema: "audit_logs",
+                table: "audit_logs",
+                column: "admin_user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_audit_logs_timestamp",
+                schema: "audit_logs",
+                table: "audit_logs",
+                column: "timestamp");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "audit_logs",
+                schema: "audit_logs");
+        }
+    }
+}
